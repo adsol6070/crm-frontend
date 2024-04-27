4a16792b704666f389a33353e9b48ae9c1e784a1
jatin goyal <adsolforweb@gmail.com>
2024-04-27 17:04:56 +0530

    Changes in blog module

--- src/pages/blogs/Edit/useEditBlogForm.tsx
+++ src/pages/blogs/Edit/useEditBlogForm.tsx
@@ -1,84 +1,45 @@
-import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
-import { blogApi } from '@/common/api';
-import { useAuthContext } from '@/common/context';
+import { blogApi, useAuthContext } from '@/common';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-interface FormData {
-    tenantID: string;
-    title: string;
-    content: string;
-    category: string;
-    selectedImage: File;
-}
-type FileType = /*unresolved*/ any
-export default function useBlogForm() {
+export default function useEditBlog() {
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [categories] = useState<string[]>([]);
-    const [selectedImage, setSelectedImage] = useState<File | null>(null);
-
     const { user } = useAuthContext();
 
-    const createBlog = useCallback(async (formData: FormData) => {
+    const editBlog = async (blogId: string, {
+        title,
+        content,
+        category,
+        blogImage,
+    }: {
+        title: string;
+        content: string;
+        category: string;
+        blogImage: File;
+    }) => {
         setLoading(true);
-        setError(null);
         try {
-            await blogApi.createPost(formData);
-            // Reset form state
-            setTitle('');
-            setContent('');
-            setSelectedCategory('');
-            setSelectedImage(null);
-            alert("Blog Created Successfully");
-            // Optionally, navigate to another page if necessary
+            const formData = new FormData();
+            formData.append('tenantID', user.tenantID);
+            formData.append('title', title);
+            formData.append('content', content);
+            formData.append('category', category);
+
+            if (blogImage) {
+                formData.append('blogImage', blogImage, blogImage.name);
+            }
+
+            // const data = await blogApi.updatePost(blogId, formData); 
+            toast.success(data.message);
         } catch (error) {
-            console.error('Error creating new blog:', error);
-            setError('Failed to create new blog. Please try again later.');
+            toast.error(error.message);
         } finally {
             setLoading(false);
         }
-    }, []);
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("image: ", selectedImage)
-
-        if (!user) {
-            setError('User must be authenticated.');
-            return;
-        }
-        const formData = new FormData();
-        formData.append('tenantID', user.tenantID);
-        formData.append('title', title);
-        formData.append('content', content);
-        formData.append('category', selectedCategory);
-        if (selectedImage) {
-            formData.append('blogImage', selectedImage, selectedImage.name);
-        }
-        await createBlog(formData);
     };
 
     return {
+        editBlog,
         loading,
-        error,
-        title,
-        content,
-        selectedCategory,
-        categories,
-        selectedImage,
-        setSelectedImage,
-        handleTitleChange: (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value),
-        handleContentChange: (value: string) => setContent(value),
-        handleCategorySelect: (category: string) => setSelectedCategory(category),
-        handleSelectedImage: (files: FileType[]) => {
-            // Ensure files array is not empty
-            if (files.length > 0) {
-                console.log(files[0]);
-                setSelectedImage(files[0]);
-            }
-        },
-        handleSubmit
     };
 }
