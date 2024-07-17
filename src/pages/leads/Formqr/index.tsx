08ae68c37d5e4dd7f386198b0617d3b278bcffa3
jatin goyal <adsolforweb@gmail.com>
2024-07-17 14:42:38 +0530

    Lead Form modifications and Lead form qr code functionality created 2

--- src/pages/leads/Formqr/index.tsx
+++ src/pages/leads/Formqr/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageBreadcrumb } from '@/components';
 import { Container, Button, Spinner } from 'react-bootstrap';
-import useFormqr from './useFormqr';
+import useFormqr from './useFormqr'; 
 import styles from './formqr.module.css'
 
 const Formqr: React.FC = () => {
@@ -12,11 +12,11 @@ const Formqr: React.FC = () => {
             <PageBreadcrumb title="Form QR" subName="Leads" />
             <Container className={styles.containerDesign}>
                 {loading ? (
-                    <div className="text-center" style={{ height: "500px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                        <Spinner animation="border" role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </Spinner>
-                    </div>
+                    <div className="text-center" style={{height: "500px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
                 ) : (
                     qrCodeUrl && (
                         <div className={`qr-container ${styles.qrcontainerDesign}`}>
