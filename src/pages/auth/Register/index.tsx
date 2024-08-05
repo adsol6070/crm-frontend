afcb3857f7779b64e1ca88dc71970c2f4d9f2d59
jatin goyal <adsolforweb@gmail.com>
2024-08-05 11:32:22 +0530

    some Login and signup UI changes

--- src/pages/auth/Register/index.tsx
+++ src/pages/auth/Register/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from 'react-bootstrap'
+import { Button, Col, Row, Spinner } from 'react-bootstrap'
 import AuthLayout from '../AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import * as yup from 'yup'
@@ -24,7 +24,7 @@ const BottomLink = () => {
 	return (
 		<Row>
 			<Col xs={12} className="text-center">
-				<p className="text-dark-emphasis" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+				<p className="text-dark-emphasis" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
 					Already have account?
 					<Button
 						variant="link"
@@ -62,13 +62,13 @@ const Register = () => {
 				.mixed()
 				.test('fileSize', 'File size is too large. Maximum size is 2MB.', (value: any) => {
 					if (!value.length) return true;
-					return value[0].size <= 2 * 1024 * 1024; 
+					return value[0].size <= 2 * 1024 * 1024;
 				}),
 		})
 	)
 
 	const onSubmit = (data: UserData) => {
-		const formData = profileImage ? { ...data, profileImage } : {...data, profileImage: null}
+		const formData = profileImage ? { ...data, profileImage } : { ...data, profileImage: null }
 		register(formData)
 	}
 
@@ -140,10 +140,16 @@ const Register = () => {
 					<div className="mb-0 d-grid text-center">
 						<Button
 							variant="primary"
-							disabled={loading}
 							className="fw-semibold"
 							type="submit">
-							Sign Up
+							{loading ? (
+								<Spinner animation="border" size="sm" />
+							) : (
+								<>
+									<i className="ri-login-circle-fill me-1" />
+									<span className="fw-bold">Sign Up</span>
+								</>
+							)}
 						</Button>
 					</div>
 				</VerticalForm>
